024e00c83cfa5c4341de370b7835ef4d100153fa
Thissutek <jonathan_yau@outlook.com>
2025-03-07 01:24:57 -0500

    Getting used to how FastHTML works added an input as a test will work out styling later

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fasthtml.common import *
 
+
 app, rt = fast_app()
 
 
 @rt("/")
 def get():
-    return Div(P("Hello World!"), hx_get="/change")
+    inp = Label("Enter Current Stock Price:", Input(id="current-stock-price", name="current-stock-price", placeholder="Current Stock Price"))
+    return (Title("Options Pricer"), 
+            Div(
+                H1("Finance Made Easy"),
+                inp
+            ))
 
 
 serve()
